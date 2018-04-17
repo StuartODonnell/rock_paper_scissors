d370cc520ae8a50d4cb7f4a627ccf23dd27cad42
stooggle <stuartodonnell@gmail.com>
2018-04-17 08:04:50 +0100

    MVP complete

--- app.rb
+++ app.rb
@@ -2,4 +2,15 @@ require("sinatra")
 require("sinatra/contrib/all") if development?
 also_reload(".models/*")
 
-require_relative(".models/game.rb")
+require_relative("./models/game.rb")
+
+get "/" do
+  erb(:home)
+end
+
+get "/game/:hand1/:hand2" do
+@game = Game.rock_paper_scissors(params[:hand1], params[:hand2])
+return "#{@game}" #hashed out to develop html and erb
+# erb(:result)
+end
+# get "/rock/pockpaperscissors/:"
--- models/game.rb
+++ models/game.rb
@@ -1,16 +1,16 @@
 class Game
 
   def self.rock_paper_scissors(hand1, hand2)
-    if hand1 = "Rock" && hand2 = "Scissors"
+    if hand1 == "Rock" && hand2 == "Scissors"
       return "Player 1 wins"
-    elsif hand1 = "Paper" && hand2 = "Rock"
+    elsif hand1 == "Paper" && hand2 == "Rock"
       return "Player 1 wins"
-    elsif hand1 = "Scissors" && hand2 = "Paper"
+    elsif hand1 == "Scissors" && hand2 == "Paper"
       return "Player 1 wins"
     elsif hand1 == hand2
       return "Draw"
     else "Player 2 wins"
-
     end
-
   end
+
+end
